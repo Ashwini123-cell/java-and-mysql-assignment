fd9ef25f22d07f5dea12bb1232ba3cebd16f3d6a
ASHWINI BASAVARAJ <202401050071@mitaoe.ac.in>
2025-07-01 15:49:41 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,12 @@
+
+public class operator {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        
+        a+=b;
+        System.out.println(a);
+
+    }
+    
+}
